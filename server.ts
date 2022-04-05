4c180a3a5c81da594b38caf3ea42d1a69969882b
kniazevgeny <kniazev-evg@ya.ru>
2022-04-05 12:34:12 +0300

    feat: get external ip (address) using localtunnel

--- server.ts
+++ server.ts
@@ -1,21 +1,7 @@
-import * as express from "express";
-import * as compression from "compression"; // compresses requests
-import * as bodyParser from "body-parser";
-import xlsx from "node-xlsx";
-// Or var xlsx = require('node-xlsx').default;
-
-/**
- * Create Express server
- */
+const express = require("express");
+const localtunnel = require("localtunnel");
 const app = express();
-
-/**
- * Express configuration.
- */
-app.set("port", 3000);
-app.use(compression());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+import xlsx from "node-xlsx";
 
 // Parse a file
 const workSheetsFromFile = xlsx.parse(`Unit_11.xlsx`);
@@ -30,25 +16,51 @@ _page.forEach((_array) => {
   });
 });
 
-app.get('/', (req, res) => {
-  res.send('<h2>🏁🏁🏁</h2>')
-})
+app.get("/", (req, res) => {
+  res.send("<h2>🏁🏁🏁</h2>");
+});
 
 // console.log(page)
-app.get('/inlineSearch', (req, res) => {
-  console.log(req.query.text)
+app.get("/inlineSearch", (req, res) => {
+  console.log(req.query.text);
   let i = page.search(req.query.text);
   res.send(
     page.slice(
       i + req.query.text.length,
-      i + req.query.text.length + page.slice(i + req.query.text.length, -1).search(" ")
+      i +
+        req.query.text.length +
+        page.slice(i + req.query.text.length, -1).search(" ")
     )
   );
-})
-
-app.listen(3000, () => {
-  console.log(("  App is running at http://localhost:%d"), 3000);
-  console.log("  Press CTRL-C to stop\n");
 });
 
-// module.exports = app;
+/*
+ * Configure localtunnel
+ */
+
+var tunnel = localtunnel(
+  3000,
+  {
+    subdomain: "kniazevgeny",
+  },
+  function (err, tunnel) {
+    if (err) {
+      console.log("error");
+    }
+
+    // the assigned public url for your tunnel
+    // i.e. https://abcdefgjhij.localtunnel.me
+    const url = tunnel.url;
+    console.log(url);
+  }
+);
+
+tunnel.on("close", function () {
+  console.log("重启");
+  tunnel;
+});
+app.use((req, res, next) => {
+  tunnel;
+});
+
+app.listen(3000);
