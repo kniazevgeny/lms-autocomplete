998d56d386b87bf49fe1f6b0ebdf8ef2c3aada43
kniazevgeny <kniazev-evg@ya.ru>
2022-04-05 22:18:34 +0300

    replace .search() by .indexOf()

--- server.ts
+++ server.ts
@@ -4,6 +4,8 @@ const app = express();
 const cors = require("cors");
 import xlsx from "node-xlsx";
 
+const isDebug = false;
+
 // Add headers before the routes are defined
 app.use(cors({ origin: "*" }));
 
@@ -16,8 +18,9 @@ const _page = workSheetsFromFile[0].data;
 let page = "";
 _page.forEach((_array) => {
   (_array as string[]).forEach((element) => {
-    // or replace \n by ' '
-    page += element.split("\n").join(" ") + " ";
+    // or replace \n and \r by ' '
+    page += element.replace(/(\r\n|\n|\r)/gm, " ").trim() + " ";
+    // if (isDebug) if (element.includes("глава (книги)")) console.log(page.slice(page.length - element.length, ));
   });
 });
 
@@ -25,9 +28,8 @@ app.get("/", (req, res) => {
   res.send("<h2>🏁🏁🏁</h2>");
 });
 
-// console.log(page)
 app.get("/inlineSearch", (req, res) => {
-  let i = page.search(req.query.text);
+  let i = page.indexOf(req.query.text.trim());
   console.log(
     req.query.text,
     page.slice(
@@ -37,43 +39,56 @@ app.get("/inlineSearch", (req, res) => {
         page.slice(i + req.query.text.length, -1).search(" ")
     )
   );
+  if (isDebug) {
+    console.log(
+      i,
+      req.query.text.length,
+      page.slice(
+        i + req.query.text.length,
+        i +
+          req.query.text.length +
+          page.slice(i + req.query.text.length, -1).indexOf(" ")
+      )
+    );
+  }
   res.send({
     data: page.slice(
       i + req.query.text.length,
       i +
         req.query.text.length +
-        page.slice(i + req.query.text.length, -1).search(" ")
+        page.slice(i + req.query.text.length, -1).indexOf(" ")
     ),
   });
 });
 
-/*
- * Configure localtunnel
- */
+if (!isDebug) {
+  /*
+   * Configure localtunnel
+   */
+  var tunnel = localtunnel(
+    3000,
+    {
+      subdomain: "kniazevgeny-1",
+    },
+    function (err, tunnel) {
+      if (err) {
+        console.log("error");
+      }
 
-var tunnel = localtunnel(
-  3000,
-  {
-    subdomain: "kniazevgeny-1",
-  },
-  function (err, tunnel) {
-    if (err) {
-      console.log("error");
+      // the assigned public url for your tunnel
+      // i.e. https://abcdefgjhij.localtunnel.me
+      const url = tunnel.url;
+      console.log(url);
     }
+  );
 
-    // the assigned public url for your tunnel
-    // i.e. https://abcdefgjhij.localtunnel.me
-    const url = tunnel.url;
-    console.log(url);
-  }
-);
-
-tunnel.on("close", function () {
-  console.log("重启");
-  tunnel;
-});
-app.use((req, res, next) => {
-  tunnel;
-});
+  tunnel.on("close", function () {
+    console.log("重启");
+    tunnel;
+  });
+  app.use((req, res, next) => {
+    tunnel;
+  });
+}
 
 app.listen(3000);
